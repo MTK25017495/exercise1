5954fbf28d429474af3f0c04428eb4580fe96be0
MTK25017495 <mavhivhatresia@gmail.com>
2025-03-17 11:37:41 +0200

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operation;
+
+/**
+ *
+ * @author mavhivha tresia
+ */
+public class Operation {
+
+    public static void main(String[] args) {
+        int x=20;
+        int y=10;
+        int addition=x+y;
+        int subtraction=x-y;
+        int multiplication=x*y;
+        int division=x/y;
+        System.out.println("addition="+addition);
+        System.out.println("subtraction="+subtraction);
+        System.out.println("multiplication="+multiplication);
+        System.out.println("division="+division);
+                
+    }
+}
